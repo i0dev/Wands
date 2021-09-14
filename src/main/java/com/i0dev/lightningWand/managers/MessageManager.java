7af53eb44084400a231968915405b55485d76b66
i0Exception <AndrewOMagnuson@gmail.com>
2021-09-14 18:03:29 -0500

    Added a message manager to send messages easier

---
+++ src/main/java/com/i0dev/lightningWand/managers/MessageManager.java
@@ -0,0 +1,74 @@
+package com.i0dev.lightningWand.managers;
+
+import com.i0dev.lightningWand.Heart;
+import com.i0dev.lightningWand.templates.AbstractManager;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class MessageManager extends AbstractManager {
+    public MessageManager(Heart heart) {
+        super(heart);
+    }
+
+
+    public String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    public String papi(String s) {
+        return s;
+    }
+
+    public String pair(String msg, Pair<String, String>... pairs) {
+        for (Pair<String, String> pair : pairs) {
+            msg = msg.replace(pair.getKey(), pair.getValue());
+        }
+        return msg;
+    }
+
+    @SafeVarargs
+    public final void msg(CommandSender sender, String msg, Pair<String, String>... pairs) {
+        sender.sendMessage(color(papi(pair(msg, pairs))));
+    }
+
+    @SafeVarargs
+    public final void msg(CommandSender sender, Collection<String> msg, Pair<String, String>... pairs) {
+        msg.forEach(s -> sender.sendMessage(color(papi(pair(s, pairs)))));
+    }
+
+    public Player getPlayer(String s) {
+        if (s.length() > 20)
+            return Bukkit.getPlayer(UUID.fromString(s));
+        return Bukkit.getPlayer(s);
+    }
+
+
+    /*
+    Credit given to EmberCM
+    Discord: Ember#1404
+    GitHub: https://github.com/EmberCM
+    */
+
+    @Getter
+    @AllArgsConstructor
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    public static class Pair<K, V> {
+        K key;
+        V value;
+
+        @Override
+        public String toString() {
+            return key + "~" + value;
+        }
+    }
+
+}
