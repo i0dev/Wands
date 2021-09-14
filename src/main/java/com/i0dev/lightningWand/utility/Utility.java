726e1af0a94c4afaf9c3ea86b80414b1cc6139db
i0Exception <AndrewOMagnuson@gmail.com>
2021-09-14 18:01:52 -0500

    Changed config manager to be a static utility class.

---
+++ src/main/java/com/i0dev/lightningWand/utility/Utility.java
@@ -0,0 +1,40 @@
+package com.i0dev.lightningWand.utility;
+
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Utility {
+
+    public static String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    public static List<String> color(List<String> ss) {
+        List<String> ret = new ArrayList<>();
+        ss.forEach(s -> ret.add(color(s)));
+        return ret;
+    }
+
+    public static ItemStack makeItem(Material material, int amount, short data, String name, List<String> lore, boolean glow) {
+        ItemStack item = new ItemStack(material, amount, data);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(color(name));
+        meta.setLore(color(lore));
+        if (glow) meta.addEnchant(Enchantment.LUCK, 1, true);
+        meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+}
