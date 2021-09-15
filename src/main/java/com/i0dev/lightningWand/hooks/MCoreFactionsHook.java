77d1f6cb0e59366e88efdc97ad1c76823d642562
i0Exception <AndrewOMagnuson@gmail.com>
2021-09-15 16:58:47 -0500

    Added a hook for MassiveCore Factions

---
+++ src/main/java/com/i0dev/lightningWand/hooks/MCoreFactionsHook.java
@@ -0,0 +1,23 @@
+package com.i0dev.lightningWand.hooks;
+
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.MPlayer;
+import com.massivecraft.massivecore.ps.PS;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class MCoreFactionsHook {
+
+    public static boolean isWilderness(Location location) {
+        return BoardColl.get().getFactionAt(PS.valueOf(location)).isNone();
+    }
+
+    public static boolean isSystemFaction(Location location) {
+        return BoardColl.get().getFactionAt(PS.valueOf(location)).isSystemFaction();
+    }
+
+    public static boolean isOwn(Location location, Player player) {
+        return BoardColl.get().getFactionAt(PS.valueOf(location)).getId().equals(MPlayer.get(player).getFaction().getId());
+    }
+
+}
