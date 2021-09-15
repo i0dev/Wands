c5731cebb5cc91c5195f135045f24a382d0670d1
i0Exception <AndrewOMagnuson@gmail.com>
2021-09-14 21:51:37 -0500

    Re-Arranged code
    Added option allowHittingPlayersWithWand
    
    Cleaned up messages
    
    Added uses option.

--- src/main/java/com/i0dev/lightningWand/config/GeneralConfig.java
+++ src/main/java/com/i0dev/lightningWand/config/GeneralConfig.java
@@ -18,19 +18,36 @@ import java.util.List;
 @ToString
 public class GeneralConfig extends AbstractConfiguration {
 
-    List<Wand> wands = Collections.singletonList(
-            new Wand("&e&lTier 1 Lightning Wand",
-                    Arrays.asList("&7Use this wand to charge creepers, or just", "&7strike lightning on the ground!", "", "&7Cooldown: &c{cooldown}", "&7Knockback Power: &c{kb}"),
-                    "BLAZE_ROD",
-                    (short) 0,
-                    true,
-                    1.0,
-                    30,
-                    "tier1",
-                    getHeart()));
-
     public GeneralConfig(Heart heart, String path) {
         this.path = path;
         this.heart = heart;
     }
+
+    boolean allowHittingPlayersWithWand = false;
+
+    List<Wand> wands = Arrays.asList(
+            new Wand("&e&lTier 1 Lightning Wand",
+                    Arrays.asList("&7Use this wand to charge creepers, or just", "&7strike lightning on the ground!", "", "&7Cooldown: &c{cooldown}&7 seconds", "&7Knockback Power: &c{kb}", "&7Remaining Uses: &c{uses}"),
+                    "BLAZE_ROD",
+                    (short) 0,
+                    true,
+                    1,
+                    30,
+                    50,
+                    "tier1",
+                    getHeart()),
+
+            new Wand("&e&lTier 2 Lightning Wand",
+                    Arrays.asList("&7Use this wand to charge creepers, or just", "&7strike lightning on the ground!", "", "&7Cooldown: &c{cooldown}&7 seconds", "&7Knockback Power: &c{kb}", "&7Remaining Uses: &c{uses}"),
+                    "BLAZE_ROD",
+                    (short) 0,
+                    true,
+                    3,
+                    15,
+                    100,
+                    "tier2",
+                    getHeart())
+
+
+    );
 }
